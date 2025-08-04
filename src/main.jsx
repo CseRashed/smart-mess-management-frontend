e3f2004e68e43910a5df3a514d8aee27b870f4c0
CseRashed <cserashedul@gmail.com>
2025-08-05 01:12:31 +0600

    correction baseURL and tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import router from './Router/Router.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AuthProvider from './AuthProvider/AuthProvider.jsx'
 import { HelmetProvider } from 'react-helmet-async'
-import './index.css'
+// import './index.css'
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/smart-mess-management-frontend'
+  plugins: [react(),tailwindcss()],
+  base:'/'
 })
+
